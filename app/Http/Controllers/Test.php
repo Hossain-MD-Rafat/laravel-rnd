8ea42c6a0ea7e315a5fb86be800ab4458aab7f6f
unknown <rafat.rafathasan18@gmail.com>
2022-05-11 23:46:32 +0600

    first commit

---
+++ app/Http/Controllers/Test.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Stichoza\GoogleTranslate\GoogleTranslate;
+
+class Test extends Controller
+{
+    public function translate()
+    {
+        $tr = new GoogleTranslate();
+        $tr->setSource();
+        $tr->setTarget('en');
+        echo "Original text: გამარჯობა მსოფლიო!";
+        echo "<br>Translated text: ";
+        echo $tr->translate('გამარჯობა მსოფლიო!');
+    }
+}
